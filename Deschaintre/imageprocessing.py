33221669302ff56495ce3a2bd1ffae11c7cca105
BBBBBbruce <bubblebruce@outlook.com>
2021-04-11 19:13:14 +0100

    add datagen

--- Deschaintre/imageprocessing.py
+++ Deschaintre/imageprocessing.py
@@ -21,6 +21,22 @@ def imagestack(path):
     #print(img.shape)
     return inputimg, reference
 
+def imagestack_img(img):
+    shape = img.shape[0]
+    inputimg = img[:,0:shape,:] #rendered 3 channels
+
+    # assemble order:
+    #albedo, specular, normal, roughness
+    reference = np.zeros([shape,shape,9])
+   
+    #print(reference[:,:,0:3].shape, img[:,shape*2:shape*3,:].shape)
+    reference[:,:,0:3] = img[:,shape*2:shape*3,:]
+    reference[:,:,3:6] = img[:,shape*4:shape*5,:]
+    reference[:,:,6:8] = img[:,shape:shape*2,1:]
+    reference[:,:,8]   = img[:,shape*3:shape*4,0]
+    #print(img.shape)
+    return inputimg, reference
+
 #=== test ===
 '''
 path = os.getcwd()+'\Deschaintre\example.png'
