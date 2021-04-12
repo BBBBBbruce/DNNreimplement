5863e944fd586952b8fe1df19610377ca609523f
BBBBBbruce <bubblebruce@outlook.com>
2021-04-12 11:04:34 +0100

    sort

--- Deschaintre/Mine/DataGen.py
+++ Deschaintre/Mine/DataGen.py
@@ -6,48 +6,32 @@ import tensorflow as tf
 from imageprocessing import imagestack, imagestack_img
 import matplotlib.pyplot as plt
 
+def DataGen(path):
+    NN_size = 256
+    batch_size = 128
 
-NN_size = 256
-batch_size = 128
+    tf.compat.v1.enable_eager_execution()
+    #path = os.getcwd()+'\Deschaintre\Dataset\Train'
+    dataset = tf.data.Dataset.list_files(path+'/*.png')
 
-tf.compat.v1.enable_eager_execution()
-print(os.getcwd())
-#path = os.getcwd()+'E:\Deschaintre\Dataset\Train'
-path = 'E:\workspace_ms_zhiyuan\DNNreimplement\Deschaintre\Dataset\Train'
+    def datagen(path_str):
+        image_string = tf.io.read_file(path_str)
+        raw_input = tf.image.decode_image(image_string,dtype=tf.float64)
+        ins,outs = imagestack_img(raw_input)
+        inputs = tf.image.random_crop(ins,  [NN_size, NN_size, 3])
+        outputs= tf.image.random_crop(outs, [NN_size, NN_size, 9])
+        return inputs, outputs
 
-dataset = tf.data.Dataset.list_files(path+'/*.png')
-
-def datagen(path_str):
-    image_string = tf.io.read_file(path_str)
-    raw_input = tf.image.decode_image(image_string,dtype=tf.float64)
-    ins,outs = imagestack_img(raw_input)
-    inputs = tf.image.random_crop(ins,  [NN_size, NN_size, 3])
-    outputs= tf.image.random_crop(outs, [NN_size, NN_size, 9])
-    return inputs, outputs
-
-dataset  = dataset.map(lambda x : tf.py_function(func = datagen, inp = [x],Tout =(tf.float64,tf.float64) ))
-#dataset = dataset.map(datagen)
-inflow = dataset.map(lambda a, b: a)
-outflow = dataset.map(lambda a, b: b)
-inbatch = inflow.batch(batch_size)
-outbatch = outflow.batch(batch_size)
-print(len(list(inbatch)))
-
-#print(dataset.element_spec)
+    dataset  = dataset.map(lambda x : tf.py_function(func = datagen, inp = [x],Tout =(tf.float64,tf.float64) ))
+    #dataset = dataset.map(datagen)
+    inflow = dataset.map(lambda a, b: a)
+    outflow = dataset.map(lambda a, b: b)
+    inbatch = inflow.batch(batch_size)
+    outbatch = outflow.batch(batch_size)
+    return inbatch, outbatch
 
 
 
-'''
-for element in dataset:
-    image_string = tf.io.read_file(element)
-    raw_input = tf.image.decode_image(image_string,dtype=tf.float64)
-    ins,outs = imagestack_img(raw_input)
-    inputs = tf.image.random_crop(ins,  [NN_size, NN_size, 3])
-    outputs= tf.image.random_crop(outs, [NN_size, NN_size, 9])    
-    plt.imshow(inputs)
-    plt.show()
-'''
-#print(dataset.element_spec)
 
 
 
